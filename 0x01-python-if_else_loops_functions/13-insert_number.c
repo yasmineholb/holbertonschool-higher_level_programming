fa106fa5f8cd3d02cdc8f54f7e8f770757b8e416
yasmineholb <1055@holbertonschool.com>
2019-12-10 13:34:19 +0100

    commit

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_node - fn
+ * @head: param
+ * @number: param
+ * Return: listint
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+}
