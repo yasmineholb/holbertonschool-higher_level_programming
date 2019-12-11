6e21ce2e86d418779f460401f9fa25e66eff652f
yasmineholb <1055@holbertonschool.com>
2019-12-11 18:02:06 +0100

    arggg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv)-1 == 0:
-    print("0 arguments.")
-elif len(sys.argv)-1 == 1:
-    print("1 argument:\n{}: {}".format(len(sys.argv)-1, sys.argv[1]))
-else:
-    print("{} arguments:\n{}".format(len(sys.argv)-1, str(sys.argv)),)
-    for arg in sys.argv:    
-        print("
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv)-1 == 0:
+        print("0 arguments.")
+    elif len(sys.argv)-1 == 1:
+        print("1 argument:\n{}: {}".format(len(sys.argv)-1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(len(sys.argv)-1))
+        for a in range(1, len(sys.argv)):
+            print("{}: {}".format(a, sys.argv[a]))
