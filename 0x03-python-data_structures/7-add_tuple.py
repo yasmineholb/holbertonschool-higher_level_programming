6dfd5d974445401da182297590debfab74d1a6d3
yasmineholb <1055@holbertonschool.com>
2019-12-15 17:24:16 +0100

    commit

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    i = tuple_a or (0, 0)
+    j = tuple_b or (0, 0)
+    if len(tuple_a) == 1:
+        i = (tuple_a[0], 0)
+    if len(tuple_b) == 1:
+        j = (tuple_b[0], 0)
+    return(i[0] + j[0], i[1] + j[1])
