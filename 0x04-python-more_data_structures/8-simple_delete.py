2b8a062b5c19e27aa9f12f4a8d59f2e708d2c3b9
yasmineholb <1055@holbertonschool.com>
2019-12-16 18:58:03 +0100

    delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for i in a_dictionary:
-        if i == key:
-            del a_dictionary[i]
-            for j in a_dictionary:
-                print("{}: {}".format(j, a_dictionary[j]))
-
+    if key in a_dictionary:
+            del a_dictionary[key]
+    return(a_dictionary)
