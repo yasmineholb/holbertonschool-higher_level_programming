76bb5884153e27a520035bfda72a6347321bbbfc
yasmineholb <1055@holbertonschool.com>
2019-12-16 18:54:07 +0100

    update function

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key not in a_dictionary:
-        a_dictionary[key] = value
-        for i in sorted(a_dictionary):
-            print("{}: {}".format(i, a_dictionary[i]))
-        
-    
+    m = {key: value}
+    a_dictionary.update(m)
+    return(a_dictionary)
