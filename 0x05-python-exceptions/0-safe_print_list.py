c9724ef2bff0fa344daedf16e81d10db6597cdb7
yasmineholb <1055@holbertonschool.com>
2020-01-07 15:52:08 +0100

    project exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for i in my_list:
+        for i in range (0, x):
+            try:
+                print("{:d}".format(i), end = "")
+            except KeyError:
+                print("hell")
