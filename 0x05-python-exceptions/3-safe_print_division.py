fd373746e5e6d582bf55591093ec7b0c2b6fb527
yasmineholb <1055@holbertonschool.com>
2020-01-07 20:20:22 +0100

    another safe

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,8 @@ def safe_print_division(a, b):
     s = 0
     try:
         s = a/b
-    except :
-        s = 0
+    except(ZeroDivisionError):
+        s = None
     finally:
         print("inside result: {}".format(s))
+        return(s)
