d5ca555f9d6aed23c208b3e1147822bae00e8a12
yasmineholb <1055@holbertonschool.com>
2020-01-09 22:21:56 +0100

    fix

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-Square = __import__('102-square').Square
+class Square:
+    __currarea = 0
 
-seq = Square(5)
-seqq = Square(6)
+    def __init__(self, size=0):
+        self.size = size
 
-if seq < seqq:
-    print("Square 5 < Square 6")
-if seq != seqq:
-    print("Square 5 != Square 6")
-if seq <= seqq:
-    print("Square 5 <= Square 6")
-if seq == seqq:
-    print("Square 5 == Square 6")
-if seq >= seqq:
-    print("Square 5 >= Square 6")
-if seq > seqq:
-    print("Square 5 > Square 6")
+    def area(self):
+        return self.__currarea
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+            self.__currarea = self.__size ** 2
