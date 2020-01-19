428e81efdbe89d3df8223dfc91a05d35e5340df9
yasmineholb <1055@holbertonschool.com>
2020-01-19 07:22:43 +0100

    comm

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,20 +5,20 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestStringMethods(unittest.TestCase):
-      """
-this is the add function
-      """
-      def max_integer1(self):
-            self.assertEqual(max_integer([1, 2, 5, 8]), 8)
+    """
+    this is the add function
+    """
+    def test_integer1(self):
+        self.assertEqual(max_integer([1, 2, 5, 8]), 8)
 
-      def max_integer2(self):
-            self.assertEqual(max_integer([-1, -9, -1000, 0]), 0)
+    def test_integer2(self):
+        self.assertEqual(max_integer([-1, -9, -1000, 0]), 0)
 
-      def max_string(self):
-            self.assertEqual(max_integer(['hh', 'hhh', 'hhhh']), 'hhhh')
+    def test_string(self):
+        self.assertEqual(max_integer(['hh', 'hhh', 'hhhh']), 'hhhh')
 
-      def max_empty(self):
-            self.assertEqual(max_integer(), None)
+    def test_empty(self):
+        self.assertEqual(max_integer(), None)
             
-      def max_empty(self):
-            self.assertEqual(max_integer([]), None)
+    def test_empty1(self):
+        self.assertEqual(max_integer([]), None)
