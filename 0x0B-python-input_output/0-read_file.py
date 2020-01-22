4b4886eae1423a2fac85a2a3e5601e7df24a4cbe
yasmineholb <1055@holbertonschool.com>
2020-01-22 16:55:21 +0100

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,6 @@
 
 def read_file(filename=""):
     """ read file fn """
-    with open(filename, mode="r", encoding="utf-8") as f:
-        print(f.read())
+    with open(filename, encoding="utf-8") as f:
+        for i in f:
+            print(i, end="")
