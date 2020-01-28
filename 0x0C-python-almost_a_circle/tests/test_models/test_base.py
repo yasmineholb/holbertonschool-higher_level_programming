277064296dc935fdf37134005f3131076f3d625b
yasmineholb <1055@holbertonschool.com>
2020-01-28 07:37:51 +0100

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,3 +4,28 @@ import unittest
 from models.base import Base
 
 class TestBase(unittest.TestCase):
+
+     def test1(self):
+          bs = Base(55)
+          self.assertEqual(bs.id, 55)
+
+     def test2(self):
+          bs = Base()
+          self.assertEqual(bs.id, 1)
+
+     def test3(self):
+          bs = Base("hello")
+          self.assertEqual(bs.id, "hello")
+
+     def test4(self):
+          bs = Base(None)
+          self.assertEqual(bs.id, 1)
+
+     def test5(self):
+          bs = Base({"yass": "hm"})
+          self.assertEqual(bs.id, {"yass": "hm"})
+
+     def test6(self):
+          self.assertTrue(len(Base.__doc__) > 0)
+
+     
