be65db40b81c6bfc3ba37c1166946cc7a4c50d42
yasmineholb <1055@holbertonschool.com>
2020-01-28 05:54:49 +0100

    add json fn

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,10 @@ class Base:
         with open("{}.json".format(cls.__name__), mode='w', encoding='utf-8')\
                 as file:
             file.write(js)
+
+    def from_json_string(json_string):
+        """ from json fn """
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return(json.loads(json_string))
