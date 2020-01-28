b5804c9a2097f77f1029b6668a6c58cca67e4c6f
yasmineholb <1055@holbertonschool.com>
2020-01-28 05:22:23 +0100

    fix base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
+this is the module base
 """
+import json
 
 
 class Base:
@@ -17,6 +19,22 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """ this is to_json fn """
         m = "[]"
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return m
+        else:
+            return(json.dumps(list_dictionaries))
+
+    def save_to_file(cls, list_objs):
+        """ this is save fn """
+        ob = []
+        if list_objs is None or len(list_objs) is 0:
+            ob = []
+        else:
+            for m in list_objs:
+                ob.append(m.to_dictionary())
+        js = Base.to_json_string(ob)
+        with open("{}.json".format(cls.__name__), mode='w', encoding='utf-8')\
+                as file:
+            file.write(js)
