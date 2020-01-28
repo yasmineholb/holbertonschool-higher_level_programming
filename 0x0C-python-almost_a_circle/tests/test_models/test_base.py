1f3fa66dccbaa397409d34af70038e5602c18724
yasmineholb <1055@holbertonschool.com>
2020-01-28 07:52:32 +0100

    test fix

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,35 +3,34 @@
 import unittest
 from models.base import Base
 
+
 class TestBase(unittest.TestCase):
-     """test"""
-     def test1(self):
-          """test """
-          bs = Base(55)
-          self.assertEqual(bs.id, 55)
+    """test"""
+    def test1(self):
+        """test """
+        bs = Base(55)
+        self.assertEqual(bs.id, 55)
 
-     def test2(self):
-          """test"""
-          bs = Base()
-          self.assertEqual(bs.id, 1)
+    def test2(self):
+        """test"""
+        bs = Base()
+        self.assertEqual(bs.id, 1)
 
-     def test3(self):
-          """test """
-          bs = Base("hello")
-          self.assertEqual(bs.id, "hello")
+    def test3(self):
+        """test """
+        bs = Base("hello")
+        self.assertEqual(bs.id, "hello")
 
-     def test4(self):
-          """test """
-          bs = Base(None)
-          self.assertEqual(bs.id, 1)
+    def test4(self):
+        """test """
+        bs = Base(None)
+        self.assertEqual(bs.id, 2)
 
-     def test5(self):
-          """test """
-          bs = Base({"yass": "hm"})
-          self.assertEqual(bs.id, {"yass": "hm"})
+    def test5(self):
+        """test """
+        bs = Base({"yass": "hm"})
+        self.assertEqual(bs.id, {"yass": "hm"})
 
-     def test6(self):
-          """test """
-          self.assertTrue(len(Base.__doc__) > 0)
-
-     
+    def test6(self):
+        """test """
+        self.assertTrue(len(Base.__doc__) > 0)
