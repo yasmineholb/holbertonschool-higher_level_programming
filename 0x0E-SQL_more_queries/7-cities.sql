4c874178e50ba58a1528b63bd83881d2fd088483
yasmineholb <1055@holbertonschool.com>
2020-03-03 20:55:13 +0100

    comm

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
 -- task 7
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-  PRIMARY KEY(id),
   id int NOT NULL AUTO_INCREMENT,
   state_id int NOT NULL,
   name varchar(256) NOT NULL,
+  PRIMARY KEY(id),
   FOREIGN KEY(state_id)
   REFERENCES hbtn_0d_usa.states(id)
 );
