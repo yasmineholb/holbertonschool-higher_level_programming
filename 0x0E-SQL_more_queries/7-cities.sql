20d8ffd46b2e65b7444ecc3d3b10faf124142f39
yasmineholb <1055@holbertonschool.com>
2020-03-03 12:44:39 +0100

    add foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- task 7
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+  id int NOT NULL AUTO_INCREMENT,
+  state_id int NOT NULL,
+  name varchar(256) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (state_id) REFERENCES states(id)
+);
