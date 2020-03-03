bf20d165c44213c8134e4c659e264f741fa71083
yasmineholb <1055@holbertonschool.com>
2020-03-03 17:25:21 +0100

    privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- task 1
-CREATE USER 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+-- PRIVILEGES
+CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
