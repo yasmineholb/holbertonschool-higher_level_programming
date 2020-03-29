d8293594def3942b1d068b982b61f09199e9a3c0
yasmineholb <1055@holbertonschool.com>
2020-03-29 19:45:32 +0200

    fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,11 +7,12 @@ if __name__ == '__main__':
                          port=3306,
                          user=argv[1],
                          password=argv[2],
-                         db=argv[3])
+                         db=argv[3]
+                         )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query = cur.fetchall()
-    for row in query():
+    for row in query:
         print(row)
     cur.close()
     db.close()
