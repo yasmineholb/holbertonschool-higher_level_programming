08fd2bf1ea4a3b7ff6f2107f7a0add9d6bc672b5
yasmineholb <1055@holbertonschool.com>
2020-03-29 23:59:08 +0200

    fix

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                            argv[2], argv[3], pool_pre_ping=True))
     Base.metadata.create_all(engine)
     session = Session(engine)
-    se = session.query(State).filter_by(id=4).first()
+    se = session.query(State).filter_by(id=2).first()
     se.name = 'New Mexico'
     session.add(se)
     session.commit()
