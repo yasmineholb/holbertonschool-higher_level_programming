d63f9ab6be31d37fd335b4ab01f6fc8540df0010
yasmineholb <1055@holbertonschool.com>
2020-04-10 04:37:34 +0200

    new fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """ pick function """
+
+
 def find_peak(list_of_integers):
+    """ peak task"""
     if len(list_of_integers) >= 2:
         if list_of_integers[0] > list_of_integers[1]:
             return list_of_integers[0]
     for i in range(1, len(list_of_integers) - 1):
-        if list_of_integers[i] > list_of_integers[i-1] and  list_of_integers[i] > list_of_integers[i+1]:
+        if list_of_integers[i] > list_of_integers[i-1] and \
+           list_of_integers[i] > list_of_integers[i+1]:
             return list_of_integers[i]
         if list_of_integers[i] == list_of_integers[i + 1]:
             return list_of_integers[i]
