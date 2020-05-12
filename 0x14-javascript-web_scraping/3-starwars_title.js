d478eb16654a7ee9b5d9cfb6227e144ee1e464ed
yasmineholb <1055@holbertonschool.com>
2020-05-12 09:17:38 -0500

    new

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,11 @@
 
 const request = require('request');
 const id = process.argv[2];
+<<<<<<< HEAD
 const url = 'https://swapi-api.hbtn.io/api/films/' + id + '/';
+=======
+const url = 'http://swapi.co/api/films/' + id;
+>>>>>>> parent of cd2bd42... verif
 
 request.get(url, function (error, resp, body) {
   if (error) {
