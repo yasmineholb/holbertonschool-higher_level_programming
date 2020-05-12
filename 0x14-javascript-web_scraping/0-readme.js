47889130ffd715ec05115b98dd4febaec5b1874e
yasmineholb <1055@holbertonschool.com>
2020-05-12 08:36:44 -0500

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data.toString());
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
 });
