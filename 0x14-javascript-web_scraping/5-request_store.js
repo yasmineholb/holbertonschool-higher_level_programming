56ae2435a95bcc3677f440e01b02cd302c49d623
yasmineholb <1055@holbertonschool.com>
2020-05-12 17:44:20 -0500

    n5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,6 +2,16 @@
 
 const fs = require('fs');
 const request = require('request');
-var url = process.argv[2];
-var pr = request(url);
-pr.pipe(fs.createWriteStream(process.argv[3]));
+const url = process.argv[2];
+const fl = process.argv[3];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(fl, body, 'utf-8', function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
